436a2e2d8fc62f102c41cfa8383e473504dbff12
Vitaliy Inshakov <michael8913@gmail.com>
2019-11-08 10:21:08 +0200

    feat(app): added trending request

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,19 @@ const express = require("express"),
     request = require("request");
 
 const router = express.Router();
+const tmdbApiKey = config.get("tmdbApiKey");
+const trendingUrl = config.get("trendingUrl");
 
 /* GET home page. */
-router.get("/", function(req, res, next) {
-  res.render("index", { title: 'Express' });
+router.get("/", function(req, res) {
+    request(`${trendingUrl}/day?api_key=${tmdbApiKey}`, (err, resp, body) => {
+        let data = JSON.parse(body);
+
+        res.render("index", {
+            movies: data.results,
+        });
+    });
+
 });
 
 module.exports = router;
