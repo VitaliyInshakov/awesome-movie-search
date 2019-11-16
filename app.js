ee4b903b311402f68e1ab5f29457f111f59b0fa2
Vitaliy Inshakov <michael8913@gmail.com>
2019-11-16 22:45:27 +0200

    fix(app): fix error fix deploy

--- app.js
+++ app.js
@@ -22,15 +22,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-if (app.get("env") === "production") {
+require("./routes/index")(app);
+
+if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
-
-require("./routes/index")(app);
 
 // error handler
 app.use(function(err, req, res, next) {
